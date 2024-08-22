6a4edb905d5b6ab0f525e6c43639a33c5f515997
milly.nguyen <milly.nguyen@qrra.com>
2024-08-22 11:01:39 +0700

    feat(TV-01): setup code base

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -2,18 +2,10 @@ import { combineReducers } from 'redux';
 // slices
 import authenticationReducer from './slices/authentication';
 import userReducer from './slices/user';
-import rolesReducer from './slices/roles';
-import memberReducer from './slices/member';
-import permissionReducer from './slices/permissions';
-import candidateReducer from './slices/hr/candidate/candidate';
 
 const rootReducer = combineReducers({
   authentication: authenticationReducer,
-  permissions: permissionReducer,
-  member: memberReducer,
-  roles: rolesReducer,
   user: userReducer,
-  candidate: candidateReducer,
 });
 
 export default rootReducer;
